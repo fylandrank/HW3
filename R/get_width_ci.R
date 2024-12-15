db6288a33195feab4102021b4afc0854eedd7026
fylandrank <dafrank@umass.edu>
2024-12-14 22:27:28 -0500

    committing for push

--- R/get_width_ci.R
+++ R/get_width_ci.R
@@ -1,10 +1,17 @@
-get_width_ci <- function(est, iso_code, CI = "95") {
+#' get_width_ci
+#'
+#' this function will give the width of the confidence interval for each year for a given country or iso code
+#'
+#' @param est tibble from est.csv
+#' @param iso_code column title for country defining identifier code
+#' @param coverage confidence interval to define width by
+#'
+#' @return tibble with column "year" and "width"
+#'
+get_width_ci <- function(est, iso_code, coverage) {
   est2 <- est %>%
     filter(iso == iso_code)
-  if (is.na(CI)) {
-    CI <- "none"
-  }
-  CI_def <- if (coverage == 95) {
+  if (coverage == 95) {
     low <- "L95"
     upp <- "U95"
   }
@@ -12,3 +19,11 @@ get_width_ci <- function(est, iso_code, CI = "95") {
     low <- "L80"
     upp <- "U80"
   }
+  CI_width <- est2[[upp]] - est2[[low]]
+  ci_width_tibble <- tibble(
+    year = est2$Year,
+    width = CI_width
+  )
+  print(ci_width_tibble)
+}
+
